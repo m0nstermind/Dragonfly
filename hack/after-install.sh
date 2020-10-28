ce62ae89dbb138a51210aacdda9bfb8ba8e3aa5a
Oleg Anastasyev <oa@odnoklassniki.ru>
2020-10-28 17:58:42 +0200

    ODKL: dragonfly-daemon rpm: restore /etc/sysconfig/dragonfly of the machine after installing rpm

--- hack/after-install.sh
+++ hack/after-install.sh
@@ -7,5 +7,8 @@ set -o pipefail
 ln -s /opt/dragonfly/df-client/dfget /usr/local/bin/dfget
 ln -s /opt/dragonfly/df-client/dfdaemon /usr/local/bin/dfdaemon
 
+# overwrite sysconfig from the package to sysconfig on the machine
+mv -f /etc/sysconfig/dragonfly.rpmsave /etc/sysconfig/dragonfly >/dev/null 2>&1 || :
+
 /usr/bin/systemctl daemon-reload
 /usr/bin/systemctl enable dragonfly
