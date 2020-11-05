ac6cb3ae1a71b96961ea4f1577c4e2269fac51ee
Oleg Anastasyev <oa@odnoklassniki.ru>
2020-11-05 18:47:27 +0200

    ODKL: dragonfly-daemon rpm: force linking commands to /usr/local/bin

--- hack/after-install.sh
+++ hack/after-install.sh
@@ -4,8 +4,8 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-ln -s /opt/dragonfly/df-client/dfget /usr/local/bin/dfget
-ln -s /opt/dragonfly/df-client/dfdaemon /usr/local/bin/dfdaemon
+ln -sf /opt/dragonfly/df-client/dfget /usr/local/bin/dfget
+ln -sf /opt/dragonfly/df-client/dfdaemon /usr/local/bin/dfdaemon
 
 # overwrite sysconfig from the package to sysconfig on the machine
 mv -f /etc/sysconfig/dragonfly.rpmsave /etc/sysconfig/dragonfly >/dev/null 2>&1 || :
